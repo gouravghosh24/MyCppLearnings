c30f2e8b518a10229db1aacccfb7fdcdffea429d
Gourav Ghosh <gouravghosh6@gmail.com>
2023-09-11 03:03:07 +0530

    Third changes push

--- Class/MyApp.cpp
+++ Class/MyApp.cpp
@@ -1,38 +1,95 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class UserData{
-    public:
+class UserData
+{
+public:
     string FName;
     string LName;
     string PNumber;
     int Age;
     bool Gender;
+
+    // Memeber Functions declaration
+    void RegForm();
+    void Profiles();
 };
 
-void LoginForm(){
-    UserData UsrData;
+// Member Function defination
 
-    cout << "Enter your Name: " << endl;
-    cin >> UsrData.FName;
-    cout << "Your Name is: " << UsrData.FName << endl;
+// Fnction for signup page
+void UserData::RegForm()
+{
+    // FormVice to enter the details from the users
+    std::cout << "________________________________________________" << std::endl;
+    std::cout << "Your College Registration Page" << std::endl;
+    std::cout << "________________________________________________" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Enter your First name: ";
+    std::cin >> FName;
+    std::cout << std::endl;
+
+    std::cout << "Enter your Last name: ";
+    std::cin >> LName;
+    std::cout << std::endl;
+
+    std::cout << "Enter your age: ";
+    std::cin >> Age;
+    std::cout << std::endl;
+
+    std::cout << "Enter your Phone Number: ";
+    std::cin >> PNumber;
+    std::cout << std::endl;
+
+    std::cout << "Gender(Male:1 & Female:0 || Enter => 1 or 0): ";
+    std::cin >> Gender;
+    std::cout << std::endl;
+
+    std::cout << "Thank You for your registration!" << std::endl;
+
+    std::cout << "________________________________________________" << std::endl;
+};
+
+// Memeber Function to Print the Form/Profile
+void UserData::Profiles()
+{
+    // Output of the Form
+    std::cout << "________________________________________________" << std::endl;
+    std::cout << "Your Registration details: " << std::endl;
+    std::cout << "________________________________________________" << std::endl;
+
+    std::cout << "Your Name: " << FName << " " << LName << std::endl;
+    std::cout << "Your Phone Number: " << PNumber << std::endl;
+    std::cout << "Your Age: " << Age << std::endl;
+    if (Gender == 1)
+    {
+        std::cout << "Gender: Male" << std::endl;
+    }
+    else if (Gender == 0)
+    {
+        std::cout << "Gender: Female" << std::endl;
+    }
+
+    else
+    {
+        std::cout << "Invalid Gender!" << std::endl;
+    }
+
+    std::cout << "Thank You!" << std::endl;
+    std::cout << "________________________________________________" << std::endl;
 }
 
-void UserDataShow(string FName, string LName, string PNumber, int Age, bool Gender){
+// Main Function Entry Point
 
-    UserData Info;
+int main()
+{
 
-    cout << "OutPut::  "<< Info.FName;
+    UserData RegiForm; // Calling the class in the main function and assigning a variable
 
-}
-
-int main(){
-
-    UserData Data;
-
-    LoginForm();
-    UserDataShow(Data.FName, Data.LName, Data.PNumber, Data.Age, Data.Gender);
+    // calling memeber Functions of class Userdata
+    RegiForm.RegForm();
+    RegiForm.Profiles();
 
     return 0;
-
 }
